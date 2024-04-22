9d42fad213a55ff8002a87947e00794b9f3d3ea8
Perecharla Babi <perecharlababi47.com>
2024-04-22 17:54:46 +0530

    evolution started

--- react_era/src/index.js
+++
@@ -1,6 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-
-const root = ReactDOM.createRoot(document.getElementById("root"));
-
-root.render(<h1>Hii Dude, Let's learn React JS</h1>);
